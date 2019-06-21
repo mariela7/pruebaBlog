d65fe95f3de62f736f62f0285b56845697116122
mariela7 <52089030+mariela7@users.noreply.github.com>
2019-06-21 18:41:42 -0500

    Blog
    Prueba de blog

---
+++ SolucionWeb/Blog/Models/TodoItemContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Blog.Models
+{
+    // Puede agregar código personalizado a este archivo. Los cambios no se pueden sobrescribir.
+    // 
+    // Si desea que Entity Framework descarte y regenere el código de
+    // automáticamente cuando modifique el esquema de modelo, agregue el
+    // base de datos al método Application_Start en el archivo Global.asax.
+    // Nota: esto destruirá la base de datos y la volverá a crear cada vez que se cambie el modelo.
+    // 
+    // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<Blog.Models.TodoItemContext>());
+    public class TodoItemContext : DbContext
+    {
+        public TodoItemContext()
+            : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<TodoList> TodoLists { get; set; }
+    }
+}
